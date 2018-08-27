fdc905790f02ccdc052d212590e6247a3d331654
j.micah.manzanares <j.micah.manzanares@gmail.com>
2018-08-27 16:27:31 -0500

    Arrays Homework

---
+++ arrays2/Source.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+int main()
+{
+	//						0    1    2    3    4    5    6
+	char myFirstName[] = { 'M', 'A', 'T', 'T', 'H', 'E', 'W' };
+	char myFirstNumber[6];
+	for (int i = 0; i <= 6; i++)
+	{
+		myFirstNumber[i] = myFirstName[i];
+		std::cout << myFirstName[i] << std::endl;
+	}
+
+	{
+		//create my array   0   1  2  3   4
+		int myArray[] = { 12, 21, 8, 7, 12 };
+		//iterate over the array
+		//compare each value to evryother value to see if its grater or less than the other value, if grater swap
+		for (int i = 0; i <= 4; i++)
+		{
+			for (int j = 0; j <= 4; i++)
+			{
+				if (myArray[i] > myArray[j])
+				{
+					int temp = myArray[i];
+					myArray[i] = myArray[j];
+					myArray[j] = temp;
+				}
+			}
+		}
+
+	}
+
+	{
+		//0 2 4 6 8 10 12 14 16 18
+		//					0  1  2  3  4  5  6   7   8   9
+		int lawaranceOfArraybia[9];
+		int j = 0;
+		for (int i = 0; i <= 19; i += 2)
+		{
+			lawaranceOfArraybia[j++] = i++;
+
+		}
+	}
+
+	{
+		int map[3][3] = { { 1, 2, 3 },
+		{ 4, 5, 6 },
+		{ 7,8,9 } };
+		for (int i = 0; i <= 2; i++)
+		{
+			for (int j = 0; j <= 2; j++)
+			{
+				std::cout << map[j][i] << std::endl;
+			}
+		}
+	}
+
+	{
+		//sol1
+		// correct
+		// wrong 
+		//correct
+
+
+
+	}
+	system("Pause");
+	return 0;
+}
